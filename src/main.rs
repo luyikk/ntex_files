17e3a7382291ada09ccb1d63cf486d503ac78d75
luyi <luyikk@126.com>
2023-05-17 11:55:43 +0800

    push

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use anyhow::Result;
+use futures::future::LocalBoxFuture;
+use futures::FutureExt;
+use ntex::web::{self, get, Error, ErrorRenderer, WebRequest, WebResponse};
 use ntex::{Middleware, Service};
-use ntex::util::BoxFuture;
-use ntex::web::{self, Error, ErrorRenderer, get, WebRequest, WebResponse};
 use ntex_files as fs;
+use ntex_identity::{CookieIdentityPolicy, Identity, IdentityService, RequestIdentity};
 use ntex_session::{CookieSession, Session};
-use ntex_identity::{Identity, CookieIdentityPolicy, IdentityService, RequestIdentity};
 use time::Duration;
 
-
-async fn index(session: Session,id: Identity) -> Result<String, Error> {
+async fn index(session: Session, id: Identity) -> Result<String, Error> {
     // access session data
-    let count= if let Some(count) = session.get::<i32>("counter")? {
-        session.set("counter", count+1)?;
+    let count = if let Some(count) = session.get::<i32>("counter")? {
+        session.set("counter", count + 1)?;
         count
     } else {
         session.set("counter", 1)?;
         0
     };
 
-    let res= if let Some(id) = id.identity() {
+    let res = if let Some(id) = id.identity() {
         format!("Welcome! {id} counter:{count}")
     } else {
         format!("Welcome! Anonymous! counter:{count}")
@@ -34,29 +34,32 @@ async fn login(id: Identity) -> web::HttpResponse {
 
 #[get("/logout")]
 async fn logout(id: Identity) -> web::HttpResponse {
-    id.forget();                      // <- remove identity
+    id.forget(); // <- remove identity
     web::HttpResponse::Ok().body("logout ok")
 }
 
 #[ntex::main]
-async fn main()->Result<()> {
-    env_logger::builder().filter_level(log::LevelFilter::Debug).init();
+async fn main() -> Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
     web::HttpServer::new(|| {
         web::App::new()
-            .wrap(IdentityCheckService)
-            .wrap(CookieSession::signed(&[1; 32]) // <- create cookie based session middleware
-                .name("session")
-                .path("/")
-                .secure(false)
-                .max_age(10)
-
+            .wrap(StaticFileIdentityCheckService)
+            .wrap(
+                CookieSession::signed(&[1; 32]) // <- create cookie based session middleware
+                    .name("session")
+                    .path("/")
+                    .secure(false)
+                    .max_age(10),
             )
             .wrap(IdentityService::new(
                 // <- create identity middleware
-                CookieIdentityPolicy::new(&[10; 32])    // <- create cookie identity policy
+                CookieIdentityPolicy::new(&[10; 32]) // <- create cookie identity policy
                     .name("auth-cookie")
                     .secure(false)
-                    .visit_deadline(Duration::days(1))))
+                    .visit_deadline(Duration::days(1)),
+            ))
             .service(fs::Files::new("/static", ".").show_files_listing())
             .service(web::resource("/").to(index))
             .service(web::resource("/login").to(login))
@@ -69,30 +72,29 @@ async fn main()->Result<()> {
     Ok(())
 }
 
+pub struct StaticFileIdentityCheckService;
 
-pub struct IdentityCheckService;
-
-impl<S> Middleware<S> for IdentityCheckService{
-    type Service = IdentityCheck<S>;
+impl<S> Middleware<S> for StaticFileIdentityCheckService {
+    type Service = StaticFileIdentityCheck<S>;
 
     #[inline]
     fn create(&self, service: S) -> Self::Service {
-        IdentityCheck{ service}
+        StaticFileIdentityCheck { service }
     }
 }
 
-pub struct IdentityCheck<S>{
-    service:S
+pub struct StaticFileIdentityCheck<S> {
+    service: S,
 }
 
-impl<S, E> Service<WebRequest<E>> for IdentityCheck<S>
-    where
-        S: Service<WebRequest<E>, Response = WebResponse, Error = web::Error>,
-        E: ErrorRenderer,
+impl<S, E> Service<WebRequest<E>> for StaticFileIdentityCheck<S>
+where
+    S: Service<WebRequest<E>, Response = WebResponse, Error = web::Error> + 'static,
+    E: ErrorRenderer,
 {
     type Response = WebResponse;
     type Error = S::Error;
-    type Future<'f> = BoxFuture<'f, Result<Self::Response, Self::Error>> where Self: 'f;
+    type Future<'f> = LocalBoxFuture<'f, Result<Self::Response, Self::Error>>;
 
     ntex::forward_poll_ready!(service);
 
@@ -101,16 +103,15 @@ impl<S, E> Service<WebRequest<E>> for IdentityCheck<S>
         Box::pin(async move {
             if req.path().starts_with("/static") {
                 if let Some(id) = req.get_identity() {
-                    log::debug!("{}",id);
+                    log::debug!("{}", id);
                     self.service.call(req).await
                 } else {
-                    Ok(req.into_response(web::HttpResponse::NotFound().finish().into_body()))
+                    Ok(req.into_response(web::HttpResponse::NotFound()))
                 }
-            }else{
+            } else {
                 self.service.call(req).await
             }
-
         })
+        .boxed_local()
     }
-
 }
